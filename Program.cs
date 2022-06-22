2bed97b8c7833b1929615604fda37fc5814d5ca0
everardo277 <rene.contrerasari@nube.unadmexico.mx>
2022-06-21 21:46:46 -0500

    Segundo commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Ejercicios  de la session uno
+
+Console.WriteLine("Ingresa tú nombre");
+string name = Console.ReadLine();
+
+Console.WriteLine("Hola envío un saludo a " + name);
+
+
+// sesion 1 ejercicio 2
+
+Console.WriteLine("Ingresa la hora ");
+string hora = Console.ReadLine();
+Console.WriteLine("La hora actual es " + hora + " am.");
